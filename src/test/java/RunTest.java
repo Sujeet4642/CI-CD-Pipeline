3e856f1761554925476f466ed1b0ee9465fb6280
Sujeet4642 <sujeet22s1990@gmail.com>
2020-11-23 23:58:13 +0530

    new test case added

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -11,14 +11,16 @@ public class RunTest {
 	}
 	
 	@Test
+	public void t_02_test_case_is_failed() {
+		 System.out.println("This test case is supposed to be failed.");
+		 Assert.assertEquals("the assertion is failed", true, false);
+	}
+	
+	@Test
 	public void t_03_test_case_is_passed() {
 		 System.out.println("This test case is supposed to be passed.");
 		 Assert.assertEquals("the assertion is failed", true, true);
 		 
 	}
-	@Test
-	public void t_03_test_case_is_failed() {
-		 System.out.println("This test case is supposed to be failed.");
-		 Assert.assertEquals("the assertion is failed", true, false);
-	}
+	
 }
